a7e7cf90c3f2c187e4ddfec2e7b632756a2edba9
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-10-25 22:03:00 +0700

    add carousel and card genre

--- client-user/src/components/HomePage.js
+++ client-user/src/components/HomePage.js
@@ -7,6 +7,7 @@ export default class HomePage extends React.Component {
     super();
 
     this.state = {
+      genres: [],
       movies: [
         {
           id: 1,
@@ -51,19 +52,56 @@ export default class HomePage extends React.Component {
     };
   }
 
+  async componentDidMount() {
+    const result = await fetch("http://localhost:3001/genre");
+    const data = await result.json();
+    this.setState({ genres: data });
+    console.log(this.state.genres);
+  }
+
   handleClick = () => {
     alert("Tertekan");
   };
 
   render() {
     return (
-      <div style={{ minHeight: "100vh", backgroundColor: "#212121", paddingTop: "100px", color: "white" }}>
+      <div style={{ minHeight: "100vh", backgroundColor: "#212121", paddingTop: "60px", color: "white" }}>
         <Navbar></Navbar>
-        <div className="container-fluid">
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner" style={{ height: "70vh" }}>
+            <div className="carousel-item active">
+              <img src="https://img.okezone.com/content/2019/05/06/206/2051975/makna-di-balik-kalimat-i-love-you-3000-di-avengers-endgame-rzLSTLrS4P.jpg" className="d-block w-100 img-fluid" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://aliefworkshop.files.wordpress.com/2020/04/sonic5.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://imgx.sonora.id/crop/133x0:1028x587/x/photo/2020/06/12/796445458.jpeg" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+        <div className="d-flex flex-row flex-wrap justify-content-center text-dark">
+          {this.state.genres.map((genre) => {
+            return (
+              <div className="card m-3" key={genre.id}>
+                <div className="card-body">{genre.name}</div>
+              </div>
+            );
+          })}
+        </div>
+        {/* <div className="container-fluid">
           {this.state.movies.map((movie) => {
             return <MoviesCard movie={movie} key={movie.id}></MoviesCard>;
           })}
-        </div>
+        </div> */}
       </div>
     );
   }
