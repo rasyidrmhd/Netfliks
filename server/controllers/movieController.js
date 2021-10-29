079c4fa883d7b83c5555ffa22c3eb951c13ab4e9
rasyidrmhd <rasyidrmuhammad@gmail.com>
2021-10-29 23:38:47 +0700

    preparation movie controller

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,5 +1,30 @@
 const { movie } = require("../models");
 
-class MovieController {}
+class MovieController {
+  static async getAllMovie(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+
+  static async getMovieById(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+
+  static async postGenre(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+
+  static async putMovieById(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+
+  static async deleteMovieById(req, res, next) {
+    try {
+    } catch (err) {}
+  }
+}
 
 module.exports = MovieController;
